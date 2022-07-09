7f938f776f56fb52efec4f7775424a157e3280dd
Meysam <mysmarterapply.it3@gmail.com>
2022-07-09 09:41:38 +0430

    Make AuthController and development Authentication's functions

---
+++ src/app/Http/Controllers/API/V01/Auth/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API\V01\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use function Illuminate\Session\userId;
+
+class AuthController extends Controller
+{
+    /**
+     * Register New User
+     * @method Post
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        // Validate Form Inputs
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required','email','unique:users'],
+            'password' => ['required']
+        ]);
+
+        // Insert User Into Database
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message'=> 'User Created Successfully'
+        ],201);
+    }
+
+    /**
+     *
+     * Login User
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required','email'],
+            'password' => ['required']
+        ]);
+
+        // Check User Credentials For Login
+        if (Auth::attempt($request->only(['email','password']))){
+            return response()->json(Auth::user(),200);
+        }
+
+        throw ValidationException::withMessages([
+            'email'=> 'incorrect credentiols.'
+        ]);
+    }
+
+    public function user()
+    {
+        return response()->json(Auth::user(),200);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            "message"=>"logout successfully"
+        ],200);
+    }
+}
