9b1c3a44dbb47f81acde2bdd0bb81bbfe550a192
Meysam <mysmarterapply.it3@gmail.com>
2022-07-09 09:38:57 +0430

    Authentication Route

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+$api_namespace = 'App\Http\Controllers\API';
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::group(['namespace' => $api_namespace], function () {
+
+    Route::group(['prefix' => 'v1', 'namespace' => 'V01'], function () {
+
+        // Authentication Routes
+        Route::group(['prefix' => 'auth/', 'namespace' => 'Auth'], function () {
+            Route::get('user', 'AuthController@user')->name("auth.user");
+            Route::post('login', 'AuthController@login')->name("auth.login");
+            Route::post('logout', 'AuthController@logout')->name("auth.logout");
+            Route::post('register', 'AuthController@register')->name("auth.register");
+        });
+    });
+
 });
