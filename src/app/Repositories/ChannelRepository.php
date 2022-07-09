2ae5cd09c8a738926991bd951e04b35d61a91e86
Meysam <mysmarterapply.it3@gmail.com>
2022-07-09 16:55:56 +0430

    Make ChannelRepository

---
+++ src/app/Repositories/ChannelRepository.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Repositories;
+
+
+use App\Models\Channel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ChannelRepository
+{
+    /**
+     * @param Request $request
+     */
+    public function create(Request $request): void
+    {
+        Channel::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function update(Request $request): void
+    {
+        Channel::find($request->id)->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name)
+        ]);
+    }
+}
