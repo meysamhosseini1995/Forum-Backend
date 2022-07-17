4f1e2cbfbb9a9d0bf80c94f8b99c6d009b3bf3b2
Meysam <mysmarterapply.it3@gmail.com>
2022-07-17 16:04:52 +0430

    Refactor Route

--- src/routes/api.php
+++ src/routes/api.php
@@ -32,12 +32,7 @@ Route::group(['namespace' => $api_namespace], function () {
         });
 
         // Channel Routes
-        Route::group(['prefix' => 'channel/'], function () {
-            Route::get('all', 'ChannelController@getAllChannelsList')->name("channels.all");
-            Route::post('create', 'ChannelController@createNewChannel')->name("channels.create");
-            Route::put('update', 'ChannelController@updateChannel')->name("channels.update");
-            Route::delete('delete', 'ChannelController@deleteChannel')->name("channels.delete");
-        });
+        Route::apiResource('/channel','ChannelController');
     });
 
 });
