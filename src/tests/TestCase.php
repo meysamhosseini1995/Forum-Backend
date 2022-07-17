d60019d1208a53e68c76e60d265a29db09632a79
Meysam <mysmarterapply.it3@gmail.com>
2022-07-17 16:30:27 +0430

    Make SeedDatabase for When Run Testing

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication , SeedDatabase;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->seedDatabase();
+    }
 }
