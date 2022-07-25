950069501ff0da3f883911c4626ddfba62181bb3
Meysam <mysmarterapply.it3@gmail.com>
2022-07-25 17:34:27 +0430

    Add Fillable To Answer Model

--- src/app/Models/Answer.php
+++ src/app/Models/Answer.php
@@ -9,6 +9,8 @@ class Answer extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id','thread_id','content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
